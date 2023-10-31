40afc20592445187228fbdc51a7af53d4f831975
Daison <daisonthms@gmail.com>
2023-10-31 18:40:48 +0530

    bigger in greater is wip

--- bigger_is_greater/src/main.rs
+++ bigger_is_greater/src/main.rs
@@ -9,16 +9,32 @@ fn main() -> std::io::Result<()> {
     let n = lines.next().unwrap().trim_end().parse::<usize>().unwrap();
 
     for _ in 0..n {
-        let w: Vec<char> = lines
-            .next()
-            .unwrap()
-            .trim_end()
-            .chars()
-            .collect::<Vec<char>>();
-        let mut sw = w.clone();
-        sw.sort();
-        println!("orginal : {w:?}");
-        println!("sorted : {sw:?}");
+        let s = lines.next().unwrap().trim_end().to_string();
+        // let w: Vec<char> = lines
+        //     .next()
+        //     .unwrap()
+        //     .trim_end()
+        //     .chars()
+        //     .collect::<Vec<char>>();
+        // let mut sw = w.as_bytes();
+        // sw.sort();
+        // println!("orginal : {w:?}");
+        // println!("bytes : {sw:?}\n");
+        // let s = input.trim().to_string();
+
+        let mut l = s.clone();
+        let mut s_chars: Vec<char> = s.chars().collect();
+
+        if next_permutation(&mut s_chars) {
+            let s = s_chars.iter().collect::<String>();
+            if s <= l {
+                println!("no answer");
+            } else {
+                println!("{}", s);
+            }
+        } else {
+            println!("no answer");
+        }
     }
 
     Ok(())
